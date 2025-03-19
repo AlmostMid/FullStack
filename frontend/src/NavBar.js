80c5b4833170279d64e23d4bdfd76cd0fa09869a
DennisErenDogulu <eren@hotmail.dk>
2025-03-19 12:24:06 +0100

    Updated AccountPage

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -7,8 +7,20 @@ function NavBar() {
     return (
         <nav style={{ marginBottom: "20px" }}>
             <Link to= "/" style={{ marginRight: "10px" }}>Home</Link>
-            <Link to="/account" style={{ marginRight: "10px" }}>Account</Link>
-
+            <Link
+        to="/account"
+        style={{
+          float: "right",
+          marginRight: "20px",
+          background: "#007bff",
+          color: "#fff",
+          padding: "6px 12px",
+          borderRadius: "4px",
+          textDecoration: "none",
+        }}
+      >
+        Account
+      </Link>
             </nav>
     );
 }
